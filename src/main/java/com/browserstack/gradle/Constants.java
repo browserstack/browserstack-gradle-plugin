e4b61d67e3475e40bb8a881928362c939f6bbaa4
Mohit Munjani <mohitcrox@gmail.com>
2018-06-05 13:45:10 +0530

    adding upload endpoints

--- src/main/java/com/browserstack/gradle/Constants.java
+++ src/main/java/com/browserstack/gradle/Constants.java
@@ -3,8 +3,11 @@ package com.browserstack.gradle;
 public class Constants {
 
     public static final String BROWSERSTACK_API_HOST = "https://api.browserstack.com",
+                                APP_LIVE_HOST = "https://app-live.browserstack.com",
+                                APP_AUTOMATE_HOST = "https://app-automate.browserstack.com",
                                 BUILD_PATH = "/app-automate/espresso/build",
-                                APP_UPLOAD_PATH = "/app-automate/upload",
+                                APP_AUTOMATE_UPLOAD_PATH = "/app-automate/upload",
+                                APP_LIVE_UPLOAD_PATH = "/app-live/upload",
                                 TEST_SUITE_UPLOAD_PATH = "/app-automate/espresso/test-suite";
 
     public static final boolean DEFAULT_VIDEO = true,
